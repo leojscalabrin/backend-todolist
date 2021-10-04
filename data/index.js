7660124350ae71bb19b822392d2f4ce93707f10f
leojscalabrin <74567440+leojscalabrin@users.noreply.github.com>
2021-10-04 16:19:59 -0300

    Init

---
+++ data/index.js
@@ -0,0 +1,40 @@
+import generateId from "../modules/generateId/index.js";
+
+const data = {
+  todoList: [],
+  set: (newData) => {
+    data.todoList = [...newData]
+  },
+
+  create: (task) => {
+    const list = data.read()
+    const id = generateId(40)
+
+    data.set([...list, {_id: id, ...task}]);
+  },
+
+  read: () => {
+    return [...data.todoList]
+  },
+
+  update: (id, newData) => {
+    const list = data.read()
+
+    const newList = list.map((item) => {
+      const data = { ...item, ...newData }
+      if (item._id === id) return data
+      if (item._id !== id) return item
+    })
+
+    data.set(newList)
+  },
+
+  delete: (id) => {
+    const list = data.read()
+    const newTodoList = list.filter((item) => item._id !== id)
+
+    data.set(newTodoList)
+  }
+};
+
+export default data
